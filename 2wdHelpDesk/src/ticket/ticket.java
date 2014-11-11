9ff88d23922f3e75be8e292edb6a816b3577e1e1
jerry <gerardoduran3@gmail.com>
2014-11-11 00:08:23 -0800

    Testing 1..2...3

---
+++ 2wdHelpDesk/src/ticket/ticket.java
@@ -0,0 +1,39 @@
+package ticket;
+
+
+import java.util.UUID;
+
+
+public class ticket
+{
+	private String Id;
+	private String title; 
+	private String description;
+	private String first;
+	private String last;
+	private String email;
+	
+   public void newTicket( ){
+	  
+	   String uuid = UUID.randomUUID().toString();
+	    Id= uuid;
+   }
+   
+   public String getTitle(){
+	   return title;
+   }
+   
+   public String getDescription() {
+        return description;
+   }
+   public String getFirst(){
+	   return first;
+   }
+   public String getLast() {
+	   return last;
+   }
+   public String getEmail() {
+	   return email;
+   }
+   
+}
