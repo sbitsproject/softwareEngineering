1afa470c33cb8db7364b566bf3235323a71afc45
praveen <pravinarr@yahoo.com>
2014-11-15 20:59:35 -0800

    Enitiy classes.

---
+++ 2wdHelpDesk/src/daoEntities/TicketAssignment.java
@@ -0,0 +1,23 @@
+package daoEntities;
+
+public class TicketAssignment {
+
+	
+	 private String ticketid;
+	 private String userid;
+	 
+	public String getTicketid() {
+		return ticketid;
+	}
+	public void setTicketid(String ticketid) {
+		this.ticketid = ticketid;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	 
+	 
+}
