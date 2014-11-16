0fa00806d0ed028d1496067c0bec8664db20d322
movleax <jitdolve@aol.com>
2014-11-15 21:49:53 -0800

    menu page updated.

--- 2wdHelpDesk/src/validation/Validation.java
+++ 2wdHelpDesk/src/validation/Validation.java
@@ -2,9 +2,11 @@ package validation;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
  
 @ManagedBean
+@SessionScoped
 public class Validation 
 {
     private connector.driver database = new connector.driver();
