01a8251f46ed15ccafed1bc3a89de63342b0abaf
praveen <pravinarr@yahoo.com>
2014-11-16 18:55:08 -0800

    DAO implementation

---
+++ 2wdHelpDesk/src/beans/SaveTicketDetailsResponse.java
@@ -0,0 +1,17 @@
+package beans;
+
+public class SaveTicketDetailsResponse {
+	
+	boolean saved;
+
+	public boolean isSaved() {
+		return saved;
+	}
+
+	public void setSaved(boolean saved) {
+		this.saved = saved;
+	}
+	
+	
+
+}
---
+++ 2wdHelpDesk/src/daoEntities/Company.java
@@ -0,0 +1,71 @@
+package daoEntities;
+
+public class Company {
+	
+	private String id;
+	private String name;
+	private String addressLine1;
+	private String addressLine2;
+	private String city;
+	private String stateName;
+	private String zipcode;
+	private String emailID;
+	private String phoneNumber;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getStateName() {
+		return stateName;
+	}
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getEmailID() {
+		return emailID;
+	}
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	
+	
+}
