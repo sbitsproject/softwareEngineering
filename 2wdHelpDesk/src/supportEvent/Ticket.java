a55196e347ebe2b20cd6a81f033ac3d10996c219
Austin <jadolve@gmail.com>
2014-12-16 15:46:49 -0800

    Ticket.java and TicketService.java to replace Car.java and CarService.java

---
+++ 2wdHelpDesk/src/supportEvent/Ticket.java
@@ -0,0 +1,71 @@
+package supportEvent;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+
+//import sunw.io.Serializable;
+
+
+public class Ticket implements Serializable {
+
+	String id = new String();
+	String Client = new String();
+	String Date = new String();
+	String Assigned = new String();
+	int Time;
+	boolean PriorityState;
+	public Ticket(String randomId, String randomClient, String randomDate,
+			String randomAssigned, int randomTime, boolean randomPriorityState) {
+		this.id=randomId;
+		this.Client=randomClient;
+		this.Date=randomDate;
+		this.Assigned=randomAssigned;
+		this.Time=randomTime;
+		this.PriorityState=randomPriorityState;
+		// TODO Auto-generated constructor stu
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getClient() {
+		return Client;
+	}
+	public void setClient(String Client) {
+		this.Client = Client;
+	}
+	public String getDate() {
+		return Date;
+	}
+	public void setDate(String Date) {
+		this.Date = Date;
+	}
+	public String getAssigned() {
+		return Assigned;
+	}
+	public void setAssigned(String Assigned) {
+		this.Assigned = Assigned;
+	}
+	public int getTime() {
+		return Time;
+	}
+	public void setTime(int Time) {
+		this.Time = Time;
+	}
+	public boolean isPriorityState() {
+		return PriorityState;
+	}
+	public void setPriorityState(boolean PriorityState) {
+		this.PriorityState = PriorityState;
+	}
+	public boolean getPriority() {
+		return PriorityState;
+	}
+	public void setPriority(boolean PriorityState) {
+		this.PriorityState = PriorityState;
+	}
+		
+}
---
+++ 2wdHelpDesk/src/supportEvent/TicketService.java
@@ -0,0 +1,88 @@
+package supportEvent;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+//import org.primefaces.showcase.domain.Ticket;
+
+
+@ManagedBean(name = "ticketService")
+@ApplicationScoped
+public class TicketService implements Serializable {
+     
+    private final static String[] assigned;
+     
+    private final static String[] client;
+     
+    static {
+    	assigned = new String[10];
+    	assigned[0] = "Andrew";
+        assigned[1] = "Larry";
+        assigned[2] = "Steve";
+        assigned[3] = "Jeff";
+        assigned[4] = "Sheldon";
+        assigned[5] = "Leonard";
+        assigned[6] = "Penny";
+        assigned[7] = "Howard";
+        assigned[8] = "Amy";
+        assigned[9] = "Will";
+         
+        client = new String[10];
+        client[0] = "Atmel";
+        client[1] = "Intel";
+        client[2] = "Microchip";
+        client[3] = "Microsystems";
+        client[4] = "Inspironix";
+        client[5] = "Najsoft";
+        client[6] = "Oracle";
+        client[7] = "Tech";
+        client[8] = "Helix Inc.";
+        client[9] = "Dijkstra";
+    }
+     
+    public List<Ticket> createTickets(int size) {
+        List<Ticket> list = new ArrayList<Ticket>();
+        for(int i = 0 ; i < size ; i++) {
+            list.add(new Ticket(getRandomId(), getRandomClient(), getRandomDate(), getRandomAssigned(), getRandomTime(), getRandomPriorityState()));
+        }
+         
+        return list;
+    }
+     
+    private String getRandomId() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+     
+    private String getRandomDate() {
+        return (int)(Math.random() * 11 + 1) + "-" + (int)(Math.random() * 28 + 1) + "-" + (2014) ;
+    }
+     
+    private String getRandomAssigned() {
+        return assigned[(int) (Math.random() * 10)];
+    }
+     
+    private String getRandomClient() {
+        return client[(int) (Math.random() * 10)];
+    }
+     
+    public int getRandomTime() {
+        return (int) (Math.random() * 100);
+    }
+     
+    public boolean getRandomPriorityState() {
+        return (Math.random() > 0.5) ? true: false;
+    }
+ 
+    public List<String> getAssigned() {
+        return Arrays.asList(assigned);
+    }
+     
+    public List<String> getClient() {
+        return Arrays.asList(client);
+    }
+}
